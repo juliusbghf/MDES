193ffc3e4463bd1f4859987c351d08219a3ff803
juliusbghf <156296878+juliusbghf@users.noreply.github.com>
2025-03-14 19:07:37 +0100

    Update 2_Results.py

--- pages/2_Results.py
+++ pages/2_Results.py
@@ -45,6 +45,7 @@ if "worker_costs_df" in st.session_state:
     st.dataframe(worker_costs_df, use_container_width=True)
 
     # Balkendiagramm der Gesamtkosten pro Arbeiter
+    worker_costs_df['Total Cost'] = worker_costs_df['Total Cost'].astype(float)
     worker_costs_chart = px.bar(worker_costs_df, x='Worker', y='Total Cost', title='Total Costs per Worker')
-    # worker_costs_chart.update_layout(yaxis=dict(range=[0, 100]))
+    worker_costs_chart.update_layout(yaxis=dict(range=[0, worker_costs_df['Total Cost'].max() * 1.1]))
     st.plotly_chart(worker_costs_chart)
