ddde4f27e3c8a583dbe91c859ce43a34641bbfdf
juliusbghf <156296878+juliusbghf@users.noreply.github.com>
2025-03-14 18:42:39 +0100

    Update 1_Initialisation.py

--- pages/1_Initialisation.py
+++ pages/1_Initialisation.py
@@ -93,10 +93,10 @@ product_C = pd.DataFrame([
 
 
 st.markdown("# Initialisation")
-st.markdown("On this page, you can visualize and initialize the data. There are three products with different sequences of setup-processes that are fixed. At first you have the option to visualize the set-up processes of each product. For each product there is a tabular and graphical representation.")
-st.markdown("In total the workforce for fulfilling the processes consists of two workers, a machine operator and a setter, that can either fulfill these tasks alone or together. For each of these options there is a processing time and a related cost.")
-st.markdown("The processes can either be internal, meaning from 'inside' of the machine, or external, meaning from 'outside' of the machine. Also included is a production process.")
-st.markdown("The graphical visualisation shows the sequence of processes with the predecessor relationships.")
+st.markdown("* On this page, you can visualize and initialize the data. There are three products with different sequences of setup-processes that are fixed. At first you have the option to visualize the set-up processes of each product. For each product there is a tabular and graphical representation.")
+st.markdown("* In total the workforce for fulfilling the processes consists of two workers, a machine operator and a setter, that can either fulfill these tasks alone or together. For each of these options there is a processing time and a related cost.")
+st.markdown("* The processes can either be internal, meaning from 'inside' of the machine, or external, meaning from 'outside' of the machine. Also included is a production process.")
+st.markdown("* The graphical visualisation shows the sequence of processes with the predecessor relationships.")
 # check if the session state for the visualisation is set
 if "visualisation" not in st.session_state:
     st.session_state.visualisation = "Product A"
